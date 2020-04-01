1accf19fee7c0e664308e312533d45481c29a64a
pavel11111111111111111111 <bob-ivanov007@mail.ru>
2020-04-01 18:19:44 +0300

    pls

---
+++ save_device_info.py
@@ -0,0 +1,19 @@
+
+import boto3
+import requests
+
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    l = boto3.client('lambda')
+
+
+
+    l.update_function_code(
+        FunctionName="check_device_info",
+        S3Bucket="forevent",
+        S3Key="save_device_info"
+    )
+
+
+    return ('test trigger')
